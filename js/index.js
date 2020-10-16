b7ff03d677f5dfdcea0edfd00a207beafbdc5629
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-16 11:05:47 +0100

    Rendering all the 52 cards

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const suit = 'hearts';
+const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 const cardsWrapper = document.querySelector('.cards-wrapper');
 const btnWrapper = document.querySelector('.btn-wrapper'); /* eslint-disable-line */
 const selectedCardsWrapper = document.querySelector('.selected-cards'); /* eslint-disable-line */
@@ -6,12 +6,15 @@ const selectedCardsWrapper = document.querySelector('.selected-cards'); /* eslin
 function createCards() {
   const cards = [];
   // Create an array with objects containing the value and the suit of each card
-  for (let i = 1; i <= 13; i += 1) {
-    const cardObject = {
-      value: i,
-      suit,
-    };
-    cards.push(cardObject);
+  for (let x = 0; x < suits.length; x += 1){
+    for (let i = 1; i <= 13; i += 1) {
+      const cardObject = {
+        value: i,
+        suit: suits[x],
+      };
+      cards.push(cardObject);
+      //console.log(cardObject)
+    }
   }
 
   // For each dataObject, create a new card and append it to the DOM

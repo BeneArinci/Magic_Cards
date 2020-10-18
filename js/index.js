8e0673e41d3cfde0123ba46f139e0e295c84ad82
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-18 01:40:28 +0100

    Committing before making the last big chanche in functions order

--- js/index.js
+++ js/index.js
@@ -4,6 +4,11 @@ const btnWrapper = document.querySelector('.btn-wrapper'); /* eslint-disable-lin
 const selectedCardsWrapper = document.querySelector('.selected-cards'); /* eslint-disable-line */
 const cards = [];
 
+function removeBtn(buttonId) {
+  const btn = document.getElementById(buttonId);
+  btn.remove();
+}
+
 function selectCard(card) {
   const selectedCards = [...selectedCardsWrapper.children];
   if (selectedCards.length === 0) {
@@ -14,18 +19,69 @@ function selectCard(card) {
   }
 }
 
+function formatCardsVisualisation(specificWrapper) {
+  const cardsToStyle = [...specificWrapper.children];
+  cardsToStyle.forEach((card, i) => {
+    const positionFromLeft = i * 30;
+    card.style.left = `${positionFromLeft}px`;
+  });
+}
+
+function addCardsAnimation() {
+  const selectedCards = [...selectedCardsWrapper.children];
+  selectedCards.forEach((card) => {
+    card.classList.add('magic');
+  });
+}
+
+function getCardValue(card) {
+  return card.classList.value.split('-').pop();
+}
+
+function performTheMagic(card) {
+  const cardValue = getCardValue(card);
+  const deck = [...cardsWrapper.children];
+  deck.forEach((notSelectedCard) => {
+    if (getCardValue(notSelectedCard) === cardValue) {
+      // console.log(notSelectedCard.classList)
+      selectedCardsWrapper.appendChild(notSelectedCard);
+      // console.log(selectedCardsWrapper)
+    }
+  });
+  formatCardsVisualisation(selectedCardsWrapper);
+  addCardsAnimation();
+  addPlayAgainBtn();
+}
+
+function addMagicBtn(selectedCard) {
+  const magicBtn = document.getElementById('magic-btn');
+  const selectedCards = [...selectedCardsWrapper.children];
+  if (magicBtn === null && selectedCards.length === 1) {
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-lg', 'btn-secondary', 'magic');
+    button.setAttribute('id', 'magic-btn');
+    button.style.margin = '5px';
+    button.innerHTML = 'Magic';
+    btnWrapper.appendChild(button);
+    button.addEventListener('click', () => {
+      performTheMagic(selectedCard);
+      removeBtn('magic-btn');
+    });
+  }
+}
+
 function listenForCardsClick() {
   const deck = [...cardsWrapper.children];
-  deck.forEach( (singleCard) => {
+  deck.forEach((singleCard) => {
     singleCard.addEventListener('click', () => {
       selectCard(singleCard);
       addMagicBtn(singleCard);
     });
-  });   
+  });
 }
 
 function populateCardsWrapper() {
-  cards.forEach((card, i) => {
+  cards.forEach((card) => {
     const cardElement = document.createElement('div');
     cardElement.setAttribute('data-value', card.value);
     cardElement.classList.add('card', `${card.suit}-${card.value}`);
@@ -53,8 +109,8 @@ function createCards() {
 // Function to clear out the initial button and create new buttons to play the game.
 function createButtons() {
   const buttons = [
-    { name: 'shuffle-btn', innerHTML: 'Shuffle' }, 
-    { name: 'flip-btn', innerHTML: 'Show/Hide' }
+    { name: 'shuffle-btn', innerHTML: 'Shuffle' },
+    { name: 'flip-btn', innerHTML: 'Show/Hide' },
   ];
   buttons.forEach((btn) => {
     const button = document.createElement('button');
@@ -63,98 +119,23 @@ function createButtons() {
     button.style.margin = '5px';
     button.innerHTML = `${btn.innerHTML}`;
     btnWrapper.appendChild(button);
-  })
-}
-
-function removeBtn(buttonId) {
-  const Btn = document.getElementById(buttonId);
-  Btn.remove();
-}
-
-function addMagicBtn(selectedCard) {
-  const magicBtn = document.getElementById('magic-btn');
-  const selectedCards = [...selectedCardsWrapper.children];
-  if (magicBtn === null && selectedCards.length === 1){
-    const button = document.createElement('button');
-    button.classList.add('btn', 'btn-lg', 'btn-secondary', 'magic');
-    button.setAttribute('id', `magic-btn`);
-    button.style.margin = '5px';
-    button.innerHTML = `Magic`;
-    btnWrapper.appendChild(button); 
-    button.addEventListener('click', () => {
-      performTheMagic(selectedCard);
-      removeBtn('magic-btn');
-    });
-  }
-}
-
-function playAgain() {
-  removeBtn('play-again');
-  removeBtn('flip-btn');
-  selectedCardsWrapper.innerHTML = '';
-  cardsWrapper.innerHTML = '';
-  cards.splice(0, cards.length);
-  startGame();
-}
-
-function addPlayAgainBtn() {
-  const button = document.createElement('button');
-  button.classList.add('btn', 'btn-lg', 'btn-secondary');
-  button.setAttribute('id', `play-again`);
-  button.style.margin = '5px';
-  button.innerHTML = `Start Again`;
-  btnWrapper.appendChild(button); 
-  button.addEventListener('click', () => playAgain());
-}
-
-function performTheMagic(card) {
-  const cardValue = getCardValue(card);
-  const deck = [...cardsWrapper.children];
-  deck.forEach((notSelectedCard) => {
-    if(getCardValue(notSelectedCard) === cardValue) {
-      //console.log(notSelectedCard.classList)
-      selectedCardsWrapper.appendChild(notSelectedCard);
-      // console.log(selectedCardsWrapper)
-    }
   });
-  formatCardsVisualisation(selectedCardsWrapper);
-  addCardsAnimation();
-  addPlayAgainBtn();
-}
-
-function addCardsAnimation() {
-  const selectedCards = [...selectedCardsWrapper.children];
-  selectedCards.forEach((card) => {
-    card.classList.add('magic');
-  });
-}
-
-function formatCardsVisualisation(specificWrapper) {
-  const cardsToStyle = [...specificWrapper.children];
-  cardsToStyle.forEach((card, i) => {
-    const positionFromLeft = i * 30;
-    card.style.left = `${positionFromLeft}px`;
-  });
-}
-
-function getCardValue(card) {
-  return card.classList.value.split('-').pop();
 }
 
 function flipCards() {
   if (cardsWrapper.classList.contains('hidden')) {
     cardsWrapper.classList.remove('hidden');
-  } else { cardsWrapper.classList.add('hidden'); };
+  } else { cardsWrapper.classList.add('hidden'); }
+}
+
+function shuffle(cardsToShuffle) {
+  cardsToShuffle.sort(() => Math.random() - 0.5);
 }
 
 function shuffleCards() {
   cardsWrapper.innerHTML = '';
   shuffle(cards);
   populateCardsWrapper(cards);
-}
-
-function shuffle(cardsToShuffle) {
-  cardsToShuffle.sort(() => Math.random() - 0.5);
 }
 
 function listenForBtnsClick() {
@@ -170,6 +151,25 @@ function startGame() {
   createButtons();
   createCards();
   listenForBtnsClick();
+}
+
+function playAgain() {
+  removeBtn('play-again');
+  removeBtn('flip-btn');
+  selectedCardsWrapper.innerHTML = '';
+  cardsWrapper.innerHTML = '';
+  cards.splice(0, cards.length);
+  startGame();
+}
+
+function addPlayAgainBtn() {
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-lg', 'btn-secondary');
+  button.setAttribute('id', 'play-again');
+  button.style.margin = '5px';
+  button.innerHTML = 'Start Again';
+  btnWrapper.appendChild(button);
+  button.addEventListener('click', () => playAgain());
 }
 
 document.getElementById('start-game').addEventListener('click', () => {
